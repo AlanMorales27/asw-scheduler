57082148e9f81fcee8bf0bf2e1adb5b0df6ba5f6
Alan Morales <alanmoralesg47@gmail.com>
2026-09-13 20:07:21 -0500

    chore: organize docker and config files

--- config/prisma.config.ts
+++ config/prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma",
-  migrations: { path: "prisma/migrations" },
+  schema: "../prisma",
+  migrations: { path: "../prisma/migrations" },
   datasource: { url: env("DIRECT_URL") },
 });
